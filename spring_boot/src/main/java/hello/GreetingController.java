823356dd1177c1118663b5e4370deea57bf856af
chenj <chenj@gzyitop.com>
2019-03-09 11:16:15 +0800

    gcp-cloud-build-maven

---
+++ spring_boot/src/main/java/hello/GreetingController.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/** GreetingController example. */
+@RestController
+public class GreetingController {
+
+  private static final String TEMPLATE = "Hello, %s! You are visitor number %s";
+  private final AtomicLong counter = new AtomicLong();
+
+  @RequestMapping("/")
+  public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format(TEMPLATE, name, counter.incrementAndGet());
+  }
+
+}
